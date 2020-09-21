d373d48b4c4288e2f53c1e639e45b3a7dc169609
Hitomi Chu <hitomichutatematsu@keio.jp>
2020-09-21 14:53:17 +0900

    Updated Sep 21

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,12 +9,77 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var count: Float = 0.0
+    var timer: Timer = Timer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet var label2: UILabel!
+ 
+    @objc func up() {
+        
+        count = count + 0.01
+        label.text = String(format: "%.2f", count)
+        
+    }
+    
+    @objc func hantei() {
+        if count >= 9.80 && count <= 10.20 {
+            print ("PERFECT")
+            label2.text = "PERFECT"
+    }
+        
+        else if count >= 9.70 && count <= 10.30 {
+            print ("GREAT")
+            label2.text = "GREAT"
+            
+        }
+        
+        else if count >= 9.50 && count <= 10.50 {
+            print ("GOOD")
+            label2.text = "GOOD"
+            
+        }
+        
+        else {
+            print ("BAD")
+            label2.text = "BAD"
+        }
+
+    }
+    
+    @IBAction func start(){
+        
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                         target: self,
+                                         selector: #selector(self.up),
+                                         userInfo: nil,
+                                         repeats: true)
+        }
+    }
+    @IBAction func stop(){
+        if timer.isValid {
+            timer.invalidate()
+            self.hantei()
+        }
+    }
+    
+    @IBAction func resetbtn() {
+        
+
+        timer.invalidate()
+        count = 0.0
+        label.text = String(count)
+        
+}
+
 
 
 }
-
